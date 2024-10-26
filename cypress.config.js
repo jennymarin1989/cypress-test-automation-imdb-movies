d3236a8a25fe4b0ce62aa53e03e9aea88b1ef880
jennymarin1989 <jennymarin1989@hotmail.comm>
2024-10-26 22:30:23 +0200

    set up env for api test and install api plugin

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: 'https://www.imdb.com',
     videoUploadOnPasses: false
+  },
+  env: {
+    pokemonApi: 'https://pokeapi.co/api/v2'
   }
-})
+});
