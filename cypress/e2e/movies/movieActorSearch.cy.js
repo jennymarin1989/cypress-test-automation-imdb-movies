fbda319fe2625c488ba62c47daeaeba6f09f7301
jennymarin1989 <jennymarin1989@hotmail.comm>
2024-10-27 11:33:40 +0100

    negative test added for movie actor search and finalize refactoring

--- cypress/e2e/movies/movieActorSearch.cy.js
+++ cypress/e2e/movies/movieActorSearch.cy.js
@@ -1,40 +1,37 @@
-import { commonElements } from '../commons/commonElements';
+import { commonElements, searchInput } from '../commons/commonElements';
 
-describe('IMDB Req_id 1.1, Access to actor profile by search and select a completed movie', () => {
+describe('IMDB Req_id 1.1, verify actor name search functionality and access to the upcoming films', () => {
   const actorElementDisplay = '[data-testid="hero__pageTitle"]';
   const actorListItem = 'div [role="listbox"]';
   const titleID = '[data-testid="hero__primary-text"]';
   const upcomingElement = '[data-testid="accordion-item-actor-upcoming-projects"]';
   const upcomingItemList = '[data-testid="accordion-item-content-container"]';
   const signInModalElement = 'div[id="logged-out-modal-v2"]';
+  const advanceSearchSection = '[data-testid="advanced-search-section"]';
 
   beforeEach(() => {
     commonElements.testInitialSetUp();
     cy.fixture('movies').as('moviesDataActorSearch');
   });
 
-  it("should be possible to access the actor's profile by entering name on searchbox", () => {
+  it("Verify search with valid actor's name and access the upcoming films", () => {
     cy.get('@moviesDataActorSearch').then((moviesDataActorSearch) => {
       //check the correct page url when loading
       cy.url().should('contain', '/');
 
-      //search for name
+      //search for valid name
       commonElements.setInputToSearch(moviesDataActorSearch.movieActor);
       cy.get(actorListItem).contains(moviesDataActorSearch.movieActor).click();
 
       // add asertion to check that not results found for empty value or not found
 
       //check actor's name search results
-      cy.get(actorElementDisplay)
-        .find('span')
-        .should('have.text', moviesDataActorSearch.movieActor);
+      cy.get(`${actorElementDisplay} span`).should('have.text', moviesDataActorSearch.movieActor);
       commonElements.checkTitleDisplay(titleID, moviesDataActorSearch.movieActor);
 
       // click on upcoming elements and click on first movie with tag completed
       cy.get(upcomingElement).scrollIntoView().click({ force: true });
-      cy.get(upcomingItemList).find('ul').should('be.visible');
-      cy.get(upcomingItemList)
-        .find('ul')
+      cy.get(`${upcomingItemList} ul`)
         .contains('li')
         .parent()
         .within(() => {
@@ -42,14 +39,25 @@ describe('IMDB Req_id 1.1, Access to actor profile by search and select a comple
         });
 
       //should get redirected to sign in
-      cy.get(signInModalElement)
-        .find('a')
+      cy.get(`${signInModalElement} a`)
         .should(
           'have.attr',
           'href',
           'https://pro.imdb.com/signup/pro/start?rf=cons_nm_filmo&ref_=tt_pub_upslb_signup'
         )
         .and('contain', moviesDataActorSearch.tryPremium);
+    });
+  });
+
+  it('verify search with empty value', () => {
+    cy.get('@moviesDataActorSearch').then((moviesDataEmptySearch) => {
+      //search for valid name
+      cy.get(searchInput).type('  {enter}');
+      commonElements.findElementAndCheckDisplay(
+        advanceSearchSection,
+        'h3',
+        moviesDataEmptySearch.searchAdvance
+      );
     });
   });
 });
